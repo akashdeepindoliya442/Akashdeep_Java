29a1c0764fcc45227b742551e6856a58a416f7e5
Akashdeep Indoliya <akashdeepindoliya442@gmail.com>
2025-08-01 18:05:47 +0530

    Updated Hello World Program With Java 21 Settings

---
+++ Day_01_In_Java.java
@@ -0,0 +1,6 @@
+//This Is My First Ever Java Code Which Is Printing "Hello World!".
+class Day_01_In_Java{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
